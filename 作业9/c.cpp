52df1f69c5b770dd7cef26bb91ec8150cdc126bc
youngstudent2 <644699280@qq.com>
2019-12-02 00:30:16 +0800

    Update c.cpp

--- 作业9/c.cpp
+++ 作业9/c.cpp
@@ -1,89 +1,98 @@
 #include<iostream>
 #include<algorithm>
 using namespace std;
-struct Edge{
-    int x,y;
-    int cost;
-    friend istream& operator>>(istream& in,Edge& e){
-        in>>e.x>>e.y>>e.cost;
-        return in;
-    }
-    void reverse_copy(const Edge& e){
-        x=e.y;
-        y=e.x;
-        cost=e.cost;
-    }
+struct Edge {
+	int x, y;
+	int cost;
+	friend istream& operator>>(istream& in, Edge& e) {
+		in >> e.x >> e.y >> e.cost;
+		--e.x; --e.y;
+		return in;
+	}
+	void build(int x, int y, int cost) {
+		this->x = x; this->y = y; this->cost = cost;
+	}
 };
-struct Harbor{
-    int cost;
-    int city;
-    friend bool operator>(const Harbor& h1,const Harbor& h2){
-        return h1.cost>h2.cost;
-    }
+class Union {
+private:
+	int *eles;
+public:
+	Union(int n) {
+		eles = new int[n];
+		for (int i = 0; i < n; i++) {
+			eles[i] = i;
+		}
+	}
+	int find(int x) {
+		int fx = eles[x];
+		if (x != fx) {
+			eles[x] = find(fx);
+		}
+
+		return eles[x];
+	}
+	void unionSet(int x, int y) {
+		int rx = find(x);
+		int ry = find(y);
+		eles[ry] = rx;
+	}
+	bool inUnion(int x, int y) {
+		return find(x) == find(y);
+	}
 };
-int findMinPath(int city,Edge* edges,int *LUT){
-    int index=LUT[city];
-    if(index==-1){
-        return 0xfffffff;
-    }
-    else return edges[index].cost;
-}
 int main()
 {
-    long long int total_cost=0;
-    Edge* edges;
-    Harbor *harbors;
-    int *LUT;
-    bool *build_harbor;
-    int n,m;
-    cin>>n>>m;
-    edges=new Edge[m<<1];
-    harbors=new Harbor[n];
-    LUT=new int[n];
-    build_harbor=new bool[n];
-    for(int i=0;i<m;i++){
-        cin>>edges[i];
-        edges[i+m].reverse_copy(edges[i]);
-    }
-    for(int i=0;i<n;i++){
-        LUT[i]=-1;
-        build_harbor[i]=true;
-        cin>>harbors[i].cost;
-        if(harbors[i].cost==-1)
-            harbors[i].cost=0xfffffff;
-        harbors[i].city=i;
-        total_cost+=harbors[i].cost;
-        //might be overflow
-    }
-    sort(harbors,harbors+n,
-    [](const Harbor& h1,const Harbor& h2)->bool{return h1>h2;});
-    sort(edges,edges+m,
-    [](const Edge& e1,const Edge& e2)->bool{
-        if(e1.x<e2.x)return true;
-        else if(e1.x>e2.x)return false;
-        else{
-            return e1.cost<e2.cost;
-        }
-    });
-    for(int currentCity=-1,i=0;i<m;i++){
-        if(currentCity<edges[i].x){
-            currentCity=edges[i].x;
-            LUT[currentCity]=i;
-        }        
-    }
-    for(int i=0;i<n;i++){
-        int min_path=findMinPath(harbors[i].city,edges,LUT);
-        if(harbors[i].cost>min_path){
-            build_harbor[harbors[i].city]=false;
-            total_cost=total_cost-harbors[i].cost+min_path;
-        }
-    }
+	long long int total_cost = 0;
+	Edge* edges;
+	int n, m;
+	cin >> n >> m;
+	Union u(n + 1);
+	edges = new Edge[m + n];
+	//input dry path
+	for (int i = 0; i < m; i++) {
+		cin >> edges[i];
+	}
+	//input water path
+	int cost;
+	int harbor_num = 0;
+	for (int i = 0; i < n; i++) {
+		cin >> cost;
+		if (cost >= 0) {
+			edges[m+harbor_num].build(i, n, cost);
+			++harbor_num;
+		}
 
-    cout<<total_cost;
+	}
 
-    
+	//sort edges
+	int edge_num = m + harbor_num;
+	sort(edges, edges + edge_num,
+		[](const Edge& e1, const Edge& e2)->bool {return e1.cost < e2.cost; }
+	);
+	//Kruskal build MST
+	int harbor_count = 0;
+	int last_harbor_cost = -1;
+	for (int i = 0; i < edge_num; i++) {
+		if (edges[i].cost < 0) {  //all the money road should be available
+			u.unionSet(edges[i].x, edges[i].y);
+			total_cost += edges[i].cost;
+		}
+		else if (!u.inUnion(edges[i].x, edges[i].y)) {
+			u.unionSet(edges[i].x, edges[i].y);
+			total_cost += edges[i].cost;
+			if (edges[i].y == n) {
+				last_harbor_cost = edges[i].cost;
+				harbor_count++;
+			}
+		}
+	}
+	//if harbor city == 1 then remove this edge cost
+	if (harbor_count == 1)
+		total_cost -= last_harbor_cost;
 
-    return 0;
+	cout << total_cost;
+	//system("pause");
+	return 0;
 }
 /*
 5 6
